8ce61d649fe570bd3bf8354c92cde6e7b8d139e9
Boulevard Cafe <boulevardcafe@Boulevards-MacBook-Pro.local>
2026-06-01 22:07:35 +0600

    added e2e test in script

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     baseURL: process.env.PLAYWRIGHT_BASE_URL ?? "http://localhost:3000",
     trace: "on-first-retry",
   },
-  projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"] } }],
+  projects: [{ name: "chromium", use: { ...devices["Desktop Chrome"], channel: 'chrome'  } }],
   webServer: process.env.CI
     ? undefined
     : {
